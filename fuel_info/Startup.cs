cfef099768d20113eee20c1bb48ebd6369d6dc8b
Mudasir Hanif Shaikh <ms03831@st.habib.edu.pk>
2019-11-19 16:31:45 +0500

    Add project files.

---
+++ fuel_info/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(fuel_info.Startup))]
+namespace fuel_info
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
